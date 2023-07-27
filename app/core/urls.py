1a47edbba04cb00474e94ffcd8138bfa9550db6d
Gean Portugal <gean.portugal@gmail.com>
2023-07-26 23:50:18 -0300

    adjust url

--- app/core/urls.py
+++ app/core/urls.py
@@ -18,11 +18,11 @@ Including another URLconf
 from card.api.viewsets import CreditCardViewSet
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import routers
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -38,15 +38,16 @@ schema_view = get_schema_view(
 
 
 router = routers.DefaultRouter()
-router.register('credit-card', CreditCardViewSet)
-
+router.register('api/v1/credit-card', CreditCardViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
-    path('api/v1/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api/v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', include(router.urls)),
+    path('api/v1/docs/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('api/v1/redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
